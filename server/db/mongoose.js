e9c9573f95b065bd52d30d9412e9f4eb46214d45
Fadli <fadli@debian>
2018-04-29 13:42:20 +0700

    fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localfadli:fadlilocal@ds163119.mlab.com:63119/todoapi');
+mongoose.connect('mongodb://localhost:27017/todoapi');
 
 module.exports = {
     mongoose
